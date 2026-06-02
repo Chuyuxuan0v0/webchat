9428d1eff7a58a3c946042c0c93463e91ddc0867
chuyxuan_mbp <mail@chuyuxuan.top>
2026-06-02 21:44:46 +0800

    chore: remove webchat-3 (will be added as submodule)

--- webchat-3/packages/shared/src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export const DEFAULT_AVATAR_COLORS = [
-  '#FF6B6B', '#4ECDC4', '#45B7D1', '#96CEB4',
-  '#FFEAA7', '#DDA0DD', '#98D8C8', '#F7DC6F',
-  '#BB8FCE', '#85C1E9', '#F1948A', '#82E0AA',
-];
-
-export const getRandomColor = (): string => {
-  return DEFAULT_AVATAR_COLORS[Math.floor(Math.random() * DEFAULT_AVATAR_COLORS.length)];
-};
